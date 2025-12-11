7db19c6fe0c44bd1a24ff642588dfceee082c699
BramaAntoro <brama4751antoro@gmail.com>
2025-12-10 21:39:39 -0500

    feat: adding a skin thickness vs sugar level graph

--- index.py
+++ index.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
 df = pd.read_csv('jeruk_balance_500.csv')
 
 bagus = df[df['kualitas'] == 'Bagus']
@@ -18,6 +17,20 @@ plt.scatter(jelek['diameter'], jelek['berat'], s=100, alpha=0.7, color='red', la
 plt.xlabel('Diamater')
 plt.ylabel("Berat")
 plt.title("Diamater vs Berat")
+plt.legend()
+plt.grid(True, linestyle="--", alpha=0.3)
+
+
+# tebal kulit vs kadar gula
+plt.figure(figsize=(6,5))
+
+plt.scatter(bagus['tebal_kulit'], bagus['kadar_gula'], s=100, alpha=0.7, color='blue', label='Bagus')
+plt.scatter(sedang['tebal_kulit'], sedang['kadar_gula'], s=100, alpha=0.7, color='orange', label='Sedang')
+plt.scatter(jelek['tebal_kulit'], jelek['kadar_gula'], s=100, alpha=0.7, color='red', label='Jelek')
+
+plt.xlabel('Tebal Kulit')
+plt.ylabel("Kadar Gula")
+plt.title("Tebal Kulit vs Kadar Gula")
 plt.legend()
 plt.grid(True, linestyle="--", alpha=0.3)
 
